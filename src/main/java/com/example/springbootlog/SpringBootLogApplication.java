dc6020691ab897929f35b6e32365f93164797286
BeobSik <tjswl756@naver.com>
2021-08-09 15:19:42 +0900

    feat: logger

--- src/main/java/com/example/springbootlog/SpringBootLogApplication.java
+++ src/main/java/com/example/springbootlog/SpringBootLogApplication.java
@@ -1,13 +1,16 @@
 package com.example.springbootlog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootLogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootLogApplication.class, args);
+        SpringApplication app=new SpringApplication(SpringBootLogApplication.class);
+        //app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
     }
 
 }
